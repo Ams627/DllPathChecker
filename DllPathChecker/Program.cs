f3afa6a571becb182179266eea4cb58fd1a7b04f
Adrian Sims <adriansims62781@gmail.com>
2018-11-14 18:00:35 +0100

    Add project files.

---
+++ DllPathChecker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+public class Program
+{
+
+    static bool MyExists(string x)
+    {
+        Console.WriteLine($"{x}");
+        return File.Exists(x);
+    }
+    public static void Main(string[] args)
+    {
+        var files = new[] { "file1.dll", "file2.dll" };
+        var paths = Environment.GetEnvironmentVariable("PATH").Split(';');
+
+        var existsList = from file in files
+                         from path in paths
+                         group MyExists($"{path}/{file}") by file into g
+                         select new
+                         {
+                             Dll = g.Key,
+                             Exists = g.Any(x => x)
+                         };
+        foreach (var entry in existsList)
+        {
+            Console.WriteLine($"dll {entry.Dll} {(entry.Exists ? "Exists" : "Does not exist")} on PATH");
+        }
+
+        var el2 = files.Select(file => new { file, exists = paths.Any(path => File.Exists(Path.Combine(path, file))) });
+
+        Console.WriteLine();
+    }
+}
+
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add DllPathChecker/Program.cs
+git add DllPathChecker/App.config
+git add DllPathChecker/Properties/AssemblyInfo.cs
+git add DllPathChecker/DllPathChecker.csproj
+git add DllPathChecker.sln
+git add .gitignore
